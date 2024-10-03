7f648e82f5f29a1851b27a46123335e418b2a175
Rahul Gupta-Iwasaki <2237985+rahulgi@users.noreply.github.com>
2024-10-03 14:37:00 -0700

    Add support for the new v3 library.

--- src/inject.ts
+++ src/inject.ts
@@ -5,9 +5,18 @@ const extensionID = script?.getAttribute("data-id") ?? "";
 
 const theWindow = window as any;
 
+// True if we're using the new JS SDK at
+// https://github.com/statsig-io/js-client-monorepo
+// vs the old deprecated libraries
+let isJsClientV3 = false;
+
 let instance = theWindow.__STATSIG_JS_SDK__?.instance;
 if (!instance) {
   instance = theWindow.__STATSIG_SDK__?.instance;
+}
+if (!instance) {
+  instance = theWindow.__STATSIG__?.instance();
+  instance && (isJsClientV3 = true);
 }
 
 if (!instance) {
@@ -15,9 +24,9 @@ if (!instance) {
 } else {
   const input = document.createElement("input");
   input.value = JSON.stringify({
-    sdkKey: instance.sdkKey,
-    userValues: instance.store.userValues,
-    user: instance.identity.user,
+    sdkKey: isJsClientV3 ? instance._sdkKey : instance.sdkKey,
+    userValues: isJsClientV3 ? instance._store._values : instance.store.userValues,
+    user: isJsClientV3 ? instance._user : instance.identity.user,
   });
   input.id = "statsig-debugger-data";
   input.type = "hidden";
