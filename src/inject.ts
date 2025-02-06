99d8a2a750d4ab4880026b4afe6123a165291ef2
“Kat <kat@statsig.com>
2025-02-05 16:58:00 -0800

    no while loop

--- src/inject.ts
+++ src/inject.ts
@@ -11,44 +11,8 @@ let instance = theWindow.__STATSIG_JS_SDK__?.instance;
 if (!instance) {
   instance = theWindow.__STATSIG_SDK__?.instance;
 }
-if (!instance) {
-  const instances = theWindow.__STATSIG__?.instances;
-
-  if (instances && Object.keys(instances).length > 1) {
-    const keys = Object.keys(instances);
-    
-    let message = "Multiple SDK instances found. Select an instance:\n\n";
-    keys.forEach((key, index) => {
-      message += `${index + 1}: ${key.slice(0,15)}...\n`;
-    });
-
-    message += "\nEnter the number of the instance you want to use, or leave blank to use the first one:";
-
-    let selectedKey = null;
-    
-    while (!selectedKey) {
-      const input = prompt(message)?.trim();
-      
-      if (input === null || input === "" || input === undefined) {
-        selectedKey = keys[0];
-        break;
-      }
-
-      const index = parseInt(input, 10) - 1;
-      if (index >= 0 && index < keys.length) {
-        if (window.confirm(`Confirm selection: ${keys[index].slice(0,15)}...?`)) {
-          selectedKey = keys[index];
-        }
-      } else {
-        alert("Invalid selection. Please enter a valid number.");
-      }
-    }
-
-    instance = theWindow.__STATSIG__?.instance(selectedKey);
-  } else {
-    instance = theWindow.__STATSIG__?.instance();
-  }
-
+if (!instance && theWindow.__STATSIG__?.instances) {
+  instance = attemptToSelectJsMonoInstances();
   instance && (isJsMonoClient = true);
 }
 
@@ -64,4 +28,38 @@ if (!instance) {
   input.id = "statsig-debugger-data";
   input.type = "hidden";
   document.body.appendChild(input);
+}
+
+function attemptToSelectJsMonoInstances() {
+  const instances = theWindow.__STATSIG__?.instances;
+  const keys = Object.keys(instances);
+
+  if (keys.length === 1) {
+    return theWindow.__STATSIG__?.instance();
+  }
+
+  let message = "Multiple SDK instances found. Select an instance:\n\n";
+  keys.forEach((key, index) => {
+    message += `${index + 1}: ${key.slice(0,15)}...\n`;
+  });
+
+  message += "\nEnter the number of the instance you want to use, or leave blank to use the first one:";
+
+  let selectedKey = null;
+    
+  const input = prompt(message)?.trim();
+
+  if (!input) {
+    selectedKey = keys[0];
+  } else {
+    const index = parseInt(input, 10) - 1;
+    if (index >= 0 && index < keys.length && window.confirm(`Confirm selection: ${keys[index].slice(0, 15)}...?`)) {
+      selectedKey = keys[index];
+    } else {
+      alert("Invalid selection. Defaulting to the first sdk Instance.");
+      selectedKey = keys[0];
+    }
+  }
+
+  return theWindow.__STATSIG__?.instance(selectedKey);
 }
